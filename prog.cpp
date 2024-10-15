b70e07a2d7acac00ba0d73d6dd9e9253e2786f5c
amongsusman <149024306+amongsusman@users.noreply.github.com>
2024-10-15 10:34:12 -0400

    Update prog.cpp

--- prog.cpp
+++ prog.cpp
@@ -2,16 +2,16 @@
 #include <cstdio>
 #include <stdio.h>
 using namespace std;
-int main() {    
+
+int main() {
     char name[100];
     int age;
     char initial;
     float grade;
-    scanf("%s\n", &name);
+    scanf("%s", &name);
     scanf("%d\n", &age);
-    scanf("%c\n", &initial);
-    scanf("%f\n", &grade);
-    scanf("\n");
+    scanf("%c", &initial);
+    scanf("%f", &grade);
     printf("Name is %s\n", name);
     printf("Age is %d\n", age);
     printf("Initial is %c\n", initial);
